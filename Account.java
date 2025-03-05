ae84b3d63cebf41247ef215bab02c9bfea569d86
ShovelTime <gousaid67@gmail.com>
2025-03-05 18:09:18 +0100

    Implementation using locks

--- Account.java
+++ Account.java
@@ -2,13 +2,18 @@
 
 package paradis.assignment2;
 
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 class Account {
 	// Instance variables.
+	//Lock that determines access to the ability to modify this account.
+	private final ReentrantReadWriteLock accountLock = new ReentrantReadWriteLock(true);
 	private final int ID;
-	private int balance;
+	private volatile Integer balance;
 
 	// Constructor.
-	Account(int id, int balance) {
+	Account(int id, int balance)
+	{
 		ID = id;
 		this.balance = balance;
 	}
@@ -19,11 +24,27 @@ class Account {
 		return ID;
 	}
 
+	void acquireWriteLock() {
+		accountLock.writeLock().lock();
+	}
+
+	void releaseWriteLock() {
+		accountLock.writeLock().unlock();
+	}
+
 	int getBalance() {
-		return balance;
+		accountLock.readLock().lock();
+		try {
+			return balance;
+		}
+		finally {
+			accountLock.readLock().unlock();
+		}
 	}
 
 	void setBalance(int balance) {
+		if(!this.accountLock.writeLock().isHeldByCurrentThread()) throw new IllegalStateException();
+
 		this.balance = balance;
 	}
 }
--- Operation.java
+++ Operation.java
@@ -6,20 +6,37 @@ class Operation implements Runnable {
 	private final int ACCOUNT_ID;
 	private final int AMOUNT;
 	private final Account account;
+	private boolean handleLock;
 
 	Operation(Bank bank, int accountId, int amount) {
 		ACCOUNT_ID = accountId;
 		AMOUNT = amount;
 		account = bank.getAccount(ACCOUNT_ID);
+		handleLock = true;
 	}
 
-	int getAccountId() {
-		return ACCOUNT_ID;
+	Account getAccount() {
+		return account;
 	}
 
 	public void run() {
-		int balance = account.getBalance();
-		balance = balance + AMOUNT;
-		account.setBalance(balance);
+
+		//Acquire the lock on the account, if it is already owned by the current thread, which may
+		account.acquireWriteLock();
+		try {
+			int balance = account.getBalance();
+			balance = balance + AMOUNT;
+			account.setBalance(balance);
+		}
+		finally {
+			account.releaseWriteLock();
+		}
+
+	}
+
+	//Allows an outside transaction to handle the lock on behalf of the operation.
+	public void setHandleLock(boolean lock)
+	{
+		handleLock = lock;
 	}
 }	
