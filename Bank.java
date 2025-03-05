32cbac1309f9c7cb69201eb2359626f5e713f591
ShovelTime <gousaid67@gmail.com>
2025-03-05 17:07:34 +0100

    Revert to base code.

--- Bank.java
+++ Bank.java
@@ -2,79 +2,23 @@
 
 package paradis.assignment2;
 
-import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.List;
+import java.util.ArrayList;
 
 class Bank {
 	// Instance variables.
 	private final List<Account> accounts = new ArrayList<Account>();
-	private final HashMap<Integer, Transaction> busyAccounts = new HashMap<>();
-	//private final ConcurrentHashMap<Transaction, HashSet<Integer>> runningTransactions = new ConcurrentHashMap<>();
-	private final ConcurrentLinkedQueue<WaitingTransaction> awaitingTransactions = new ConcurrentLinkedQueue<>();
-	
+
 	// Instance methods.
 
 	int newAccount(int balance) {
 		int accountId = accounts.size();
-		Account account = new Account(accountId, balance, this);
-		accounts.add(account);
-		//Create lock for account.
-		locks.add(new ReentrantReadWriteLock(true));
+		accounts.add(new Account(accountId, balance));
 		return accountId;
 	}
-	
+
 	Account getAccount(int accountId) {
-		Account account;
-		account = accounts.get(accountId);
+		Account account = accounts.get(accountId);
 		return account;
 	}
-
-	boolean requestTransaction(HashMap<Integer, ArrayList<FutureTask<Void>>> requestedAccount, Transaction caller)
-	{
-		synchronized(busyAccounts)
-		{
-			if(areAccountsAvailable(requestedAccount.keySet()))
-			{
-
-				return true;
-			}
-		}
-
-		awaitingTransactions.add(new WaitingTransaction(caller, requestedAccount)); //Unable to lock all of the requested accounts, queue it for later.
-		return false;
-	}
-
-	void runTransaction(Transaction caller, HashMap<Integer, ArrayList<FutureTask<Void>>> tasks)
-	{
-		synchronized (busyAccounts)
-		{
-			for(Integer account : tasks.keySet())
-			{
-				busyAccounts.put(account, caller);
-			}
-		}
-	}
-
-	void tryAssignFromQueue()
-	{
-		if(awaitingTransactions.isEmpty()) return;
-	}
-
-	private boolean areAccountsAvailable(Set<Integer> targetedAccounts)
-	{
-		for(Integer desiredAccount: targetedAccounts)
-		{
-			if (busyAccounts.containsKey(desiredAccount)) return false;
-		}
-		return true;
-	}
-
-}
-
-record WaitingTransaction(Transaction source, HashMap<Integer, ArrayList<FutureTask<Void>>> tasks)
-{
-
 }
--- Transaction.java
+++ Transaction.java
@@ -2,56 +2,28 @@
 
 package paradis.assignment2;
 
-import java.util.*;
-import java.util.concurrent.FutureTask;
+import java.util.ArrayList;
+import java.util.List;
 
 class Transaction implements Runnable {
 	private List<Operation> operations = new ArrayList<Operation>();
 	private boolean closed = false;
-	
+
 	void add(Operation operation) {
 		if (closed) return;
 		operations.add(operation);
 	}
-	
+
 	void close() {
 		closed = true;
 	}
-	
+
 	public void run() {
 		if (!closed) return;
-		//Construct Tasks
-		//HashMap<Integer, ArrayList<FutureTask<Void>>> tasks = new HashMap<>();
-		//HashMap<Bank, HashSet<Integer>> targetedBanks = new HashMap<>();
-		HashMap<Bank, HashMap<Integer, ArrayList<FutureTask<Void>>>> tasks = new HashMap<>();
-		//ArrayList<FutureTask<Void>> tasks = new ArrayList<>(operations.size());
 
-		for(Operation operation : operations)
-		{
-			Bank bank = operation.getBank();
-			HashMap<Integer, ArrayList<FutureTask<Void>>> bankMap = tasks.getOrDefault(bank, new HashMap<>());
-
-			Integer accountID = operation.getAccountId();
-			FutureTask<Void> task = new FutureTask<>(operation, null);
-			ArrayList<FutureTask<Void>> queue = bankMap.getOrDefault(accountID, new ArrayList<>());
-			queue.add(task);
-
-			bankMap.putIfAbsent(accountID, queue);
-			tasks.putIfAbsent(bank, bankMap);
-
+		// Execute the operations.
+		for (Operation operation : operations) {
+			operation.run();
 		}
-
-		for(Map.Entry<Bank, HashMap<Integer, ArrayList<FutureTask<Void>>>> maps : tasks.entrySet())
-		{
-			maps.getKey().requestTransaction(maps.getValue(), this);
-		}
-
-
-
-
-
-		// Queue the operations for execution.
-
 	}
-
 }	
